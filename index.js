fe89114e62cf10dcb3a2e54def7cc12c032833a5
Zhang,Ryan <Ryan.Zhang@colostate.edu>
2020-01-18 00:28:29 +0000

    Build 2

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const moment = require('moment');
 const express = require('express');
 const app = require('express')();
 const path = require('path');
+const credentials = require(path.join(__dirname, 'secure', 'credentials.json')); // secure credentials
 
-// const session = require('express-session')
+const session = require('express-session');
+const sharedsession = require("express-socket.io-session");
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -37,7 +39,40 @@ const logger = winston.createLogger({
   ]
 });
 
+const MongoClient = require('mongodb').MongoClient;
+const MongoURL = credentials.database;
+const dbName = 'opentrivia';
+const dbClient = new MongoClient(MongoURL, {useNewUrlParser: true, useUnifiedTopology: true}); 
+let mdb; 
+dbClient.connect((e) => {
+  if(e){logger.error('Failed to connect to local MongoDB server', e); require('process').exit()}
+  logger.info('Connected to local MongoDB server.')
+  mdb = dbClient.db(dbName);
+})
+
+let sess_MongoStore = require('connect-mongo')(session); 
+let sess = {
+  secret: credentials.sessionKey,
+  store: new sess_MongoStore({
+    url: credentials.database, 
+    autoRemove: 'interval', 
+    autoRemoveInterval: 5
+  }),
+  saveUninitialized: false, 
+  resave: false,
+  cookie: {}
+}
+ 
+if (app.get('env') === 'production' || fs.existsSync(path.join(__dirname, 'production.cfg'))) {
+  logger.info('Production environment detected.')
+  app.set('trust proxy', 1) // trust first proxy
+  // sess.cookie.secure = true // serve secure cookies (currently not working)
+}
+
+app.use(session(sess));
+
 const io = require('socket.io')(http);
+io.use(sharedsession(session(sess))); 
 
 // End of init
 
@@ -55,11 +90,24 @@ nsp.use(function(socket, next){
       next(new Error('Authentication error'));
   }    
 }).on('connection', function(socket){
-  console.log('[secure] a user connected');
+  console.log('[secure] user connected: '+socket.id);
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  console.log('[std] user connected: '+socket.id);  
+  socket.on('auth', function(team) {
+    socket.handshake.session.team = team;
+    socket.handshake.session.save();
+  });
+  socket.on('status', function(){
+    console.log('status for: '+socket.id);
+    console.log(socket.handshake.session)
+    socket.emit(socket.handshake.session); 
+  })
+  socket.on('chat', function(msg){
+    console.log('chat: ' + msg); 
+    io.emit('chat', msg);
+  });
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
--- public/script.js
+++ public/script.js
@@ -1,5 +1,18 @@
 var socket = io();
 
 var secSocket = io('/secure', {
-    query: 'token=potato'
+  query: 'token=potato'
 })
+
+socket.on('connect', () => {
+  console.log(socket.id); // 'G5p5...'
+});
+
+socket.on('chat', function(msg){
+  console.log(msg);
+  $('#log').append($('<li>').text(msg));
+});
+
+socket.on('page', (data) => {
+  $('#log').append($('<li>').text(JSON.stringify(data))); 
+});
